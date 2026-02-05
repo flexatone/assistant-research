ae47ae8095cdf851e14c651ef8552e7e5f768fcf
flexatone <ariza@flexatone.com>
2026-02-05 11:43:24 -0800

    updated Shuntly config

--- src/anthropic_client.py
+++ src/anthropic_client.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import os
 from anthropic import Anthropic
-from shuntly import Shuntly, SinkStream
+from shuntly import Shuntly, SinkFile
 
 
 class AnthropicClientBase:
@@ -14,4 +14,4 @@ class AnthropicClientBase:
         """
         self.client = Anthropic(api_key=api_key)
         if os.getenv("GITHUB_ACTIONS") != "true" and os.getenv("CI") != "true":
-            self.client = Shuntly.shunt(self.client, SinkStream())
+            self.client = Shuntly.shunt(self.client, SinkFile('/tmp/shuntly.json'))
